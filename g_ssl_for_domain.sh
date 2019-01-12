5b6bf3c8d2fae324a49ce445d4e2a4f626f2d0a3
scottming <therealscottming@gmail.com>
2019-01-12 10:17:28 +0800

    Make more useful

--- g_ssl_for_domain.sh
+++ g_ssl_for_domain.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ] || [ -z "$2" ]; then
 else
     echo "generate crt file for $1"
     echo "------------------------"
+    mkdir -p $2
     sed "s/localhost/$1/" v3.ext > v3.ext1
 	openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
 	openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext1
