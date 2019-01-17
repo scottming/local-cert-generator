d6cc254d11d7a6ac09592de51ca86bdfa267fb19
scottming <therealscottming@gmail.com>
2019-01-17 11:17:10 +0800

    Fix path bug

--- g_ssl_for_domain.sh
+++ g_ssl_for_domain.sh
@@ -7,12 +7,12 @@ else
     echo "------------------------"
     mkdir -p $2
     sed "s/localhost/$1/" v3.ext > v3.ext1
-	openssl req -new -sha256 -nodes -out server.csr -newkey rsa:2048 -keyout server.key -config server.csr.cnf
-	openssl x509 -req -in server.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out server.crt -days 500 -sha256 -extfile v3.ext1
+	openssl req -new -sha256 -nodes -out ssl.csr -newkey rsa:2048 -keyout ssl.key -config server.csr.cnf
+	openssl x509 -req -in ssl.csr -CA rootCA.pem -CAkey rootCA.key -CAcreateserial -out ssl.crt -days 500 -sha256 -extfile v3.ext1
     echo "------------------------"
 	echo "mv to $2"
 	mkdir -p /tmp/ssl
-	mv server.crt server.csr server.key "$2"
+	mv ssl.crt ssl.csr ssl.key "$2"
 fi
 
 
